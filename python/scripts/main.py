d77c57f15e462b6e0bd811e472a2a5992a80e23a
Arsenoks4132 <Danov2005@yandex.ru>
2024-10-08 11:35:47 +0300

    done

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -1,9 +1,34 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="db",
-  user="root",
-  password="1234"
+    host="db",
+    user="test_user",
+    password="test_pass",
+    database="db_name"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+while True:
+    sql = input()
+    if sql.lower() in ('exit', 'quit'):
+        break
+    if len(sql) <= 0:
+        continue
+
+    try:
+        mycursor.execute(sql)
+    except:
+        print("wrong query")
+
+    try:
+        mydb.commit()
+    except:
+        pass
+
+    try:
+        myresult = mycursor.fetchall()
+        for i in myresult:
+            print(i)
+    except:
+        print("read failed")
